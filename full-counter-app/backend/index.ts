c3d426127f4799a3e33e3ce08b693724e28a2f9e
davidshur <shurdave@gmail.com>
2021-02-13 16:52:15 -0800

    update server to hold mongodb data

--- full-counter-app/backend/index.ts
+++ full-counter-app/backend/index.ts
@@ -1,17 +1,64 @@
-import express from 'express';
-import path from 'path';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { Collection, MongoClient, MongoError } from 'mongodb';
 
-const app = express();
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+async function startServer() {
+  if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+  }
 
-if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) => {
-    res.send("Express + TypeScript Server!");
+  const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
+  const db = client.db('counter-db');
+
+  db.collection('Count', (err: MongoError, collection: Collection) => {
+    collection.insertOne({ id: 0, name: 'count', count: 0 })
+  });
+
+  const app = express();
+
+  app.get('/api/v0/increment', (req: Request, res: Response) => {
+    db.collection('Count', async (err: MongoError, collection: Collection) => {
+      if (err) throw err;
+      const updatedCountEntry = await collection.findOneAndUpdate(
+        { id: 0 }, 
+        { $inc: { count: 1 }}, 
+        { returnOriginal: false });
+      res.send(updatedCountEntry);
+    });
+  });
+
+  app.get('/api/v0/decrement', (req: Request, res: Response) => {
+    db.collection('Count', async (err: MongoError, collection: Collection) => {
+      if (err) throw err;
+      const updatedCountEntry = await collection.findOneAndUpdate({ id: 0 }, { $inc: { count: -1 } }, { returnOriginal: false });
+      res.send(updatedCountEntry);
+    });
+  });
+
+  app.get('/api/v0/reset', (req: Request, res: Response) => {
+    db.collection('Count', async (err: MongoError, collection: Collection) => {
+      if (err) throw err;
+      const updatedCountEntry = await collection.findOneAndUpdate({ id: 0 }, { $set: { count: 0 }}, { returnOriginal: false });
+      res.send(updatedCountEntry);
+    });
+  });
+
+  app.get('*', (req, res) => {
+    res.send(`
+      <h1>Counter App API Server</h1>
+      <h3>Routes</h3>
+      <ul>
+        <li>GET: /api/v0/increment</li>
+        <li>GET: /api/v0/decrement</li>
+        <li>GET: /api/v0/reset</li>
+      </ul>
+    `);
+  });
+
+  const PORT = process.env.PORT;
+  app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
   });
 }
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+startServer();
